9d7608cd81f3e7b59c721793b7c9bc63a69aa4a1
AmanKhan2376 <amanjkhan21@gmail.com>
2025-08-04 22:11:53 +0530

    Add Optimal Solution for Peak Index in Mountain Array problem

---
+++ binary_search/peak_index_in_mountain_array_using_binary_search.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int peakIndexInMountainArray(vector<int>& arr) {
+    int start = 1, end = arr.size() - 2;
+    while(start <= end) {
+        int mid = start + ((end - start) / 2);
+        if(arr[mid - 1] < arr[mid] && arr[mid] > arr[mid + 1]) {
+            return mid;
+        } else if(arr[mid - 1] < arr[mid]) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+    }
+    return 0;
+}
+
+int main() {
+    vector<int> arr = {0, 1, 0};
+    cout << peakIndexInMountainArray(arr);
+}
